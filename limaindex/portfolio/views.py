4e927c161dbba7c10fd21dd2dff9e462db2bfe0f
Edvaldo <edliju1997@outlook.com>
2017-08-31 20:52:42 -0300

    views

--- limaindex/portfolio/views.py
+++ limaindex/portfolio/views.py
@@ -4,6 +4,6 @@ from .models import Portfolio
 
 def index(request):
     home = Portfolio.objects.all()
-    template_name = 'core/index.html'
+    template_name = 'frontend/index.html'
     context = {'index': home}
     return render(request, template_name, context)
